bb7e1f12eb16cdd48ba79cf1b8f1b8290a86e1e2
Apoorv Yadav <apoorv2yadav@gmail.com>
2021-05-21 16:01:15 +0530

    heroku

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const helmet = require("helmet");
 const express = require("express");
 const app = express();
-const port = 5000; //custom port
+// process.env.PORT will given by deployment(heroku) itself
+const port = process.env.PORT || 5000; //custom port
 app.use(cors());
 // local imports
 const registerRoute = require("./routes/register");
